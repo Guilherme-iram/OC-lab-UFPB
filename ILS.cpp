4290ab3116ed6b77114e5db5cc839f91b0555e23
Guilherme Iram <guilherme.iram@gmail.com>
2022-05-05 12:29:51 -0300

    Melhoria na classe solucao | ainda em prototipacao

--- ILS.cpp
+++ ILS.cpp
@@ -45,6 +45,81 @@ class Solution{
 
         }
 
+        void construcao(){ 
+
+            Solution s({0, 2, 1});
+            std::vector<int> caminhos_disponiveis = {3, 4, 5};
+
+            int i, j, i_menor;
+            int custo_atual, menor_custo;
+            int maximo = caminhos_disponiveis.size();
+            
+            std::vector<std::vector<int>> arestas;
+            std::vector<int> custo_aresta;
+            std::vector<int> aux;
+            
+            for (int k = 0; k < maximo; k++){
+
+                arestas.clear();
+                custo_aresta.clear();
+                
+                for (i = 0; i < caminhos_disponiveis.size(); i++){
+                    for (j = 0; j < s.sequencia.size() - 1; j++){
+                        
+                        aux.clear();
+                            
+                        custo_atual += matrizAdj[s.sequencia[j]][caminhos_disponiveis[i]];
+                        custo_atual += matrizAdj[s.sequencia[j + 1]][caminhos_disponiveis[i]];
+                        custo_atual -= matrizAdj[s.sequencia[j]][s.sequencia[j + 1]];
+
+                        aux.push_back(j);
+                        aux.push_back(caminhos_disponiveis[i]);
+                        aux.push_back(j + 1);
+
+                        arestas.push_back(aux);
+                        custo_aresta.push_back(custo_atual);
+
+                        custo_atual = 0;
+                    }
+
+                    int cont = 0;
+
+                    for (int custo : custo_aresta){
+                        if (cont == 0){
+                            menor_custo = custo;
+                            i_menor = cont;
+                        }
+
+                        if (menor_custo > custo){
+                        menor_custo = custo;
+                        i_menor = cont;
+                        }
+
+                        cont++;
+
+                    }
+                    
+                }
+            
+                s.sequencia.insert(s.sequencia.begin() + arestas[i_menor][0], arestas[i_menor][1]);
+
+                int indice_para_excluir;
+
+                for (i = 0; i < caminhos_disponiveis.size(); i++){
+                    if (arestas[i_menor][1] == caminhos_disponiveis[i]){
+                        indice_para_excluir = i;
+                        break;
+                    }
+                }
+
+                caminhos_disponiveis.erase(caminhos_disponiveis.begin() + indice_para_excluir);
+
+            }
+
+            this->sequencia = s.sequencia;
+
+        } 
+
         void movimento(int a, int b){
 
             int aux, i_a = -1, i_b = - 1;
@@ -119,81 +194,6 @@ class Solution{
 
 };
 
-Solution Construcao(){ 
-    
-    Solution s({0, 2, 1});
-    std::vector<int> caminhos_disponiveis = {3, 4, 5};
-
-    int i, j, i_menor;
-    int custo_atual, menor_custo;
-    int maximo = caminhos_disponiveis.size();
-    
-    std::vector<std::vector<int>> arestas;
-    std::vector<int> custo_aresta;
-    std::vector<int> aux;
-    
-    for (int k = 0; k < maximo; k++){
-
-        arestas.clear();
-        custo_aresta.clear();
-        
-        for (i = 0; i < caminhos_disponiveis.size(); i++){
-            for (j = 0; j < s.sequencia.size() - 1; j++){
-                
-                aux.clear();
-                    
-                custo_atual += matrizAdj[s.sequencia[j]][caminhos_disponiveis[i]];
-                custo_atual += matrizAdj[s.sequencia[j + 1]][caminhos_disponiveis[i]];
-                custo_atual -= matrizAdj[s.sequencia[j]][s.sequencia[j + 1]];
-
-                aux.push_back(j);
-                aux.push_back(caminhos_disponiveis[i]);
-                aux.push_back(j + 1);
-
-                arestas.push_back(aux);
-                custo_aresta.push_back(custo_atual);
-
-                custo_atual = 0;
-            }
-
-            int cont = 0;
-
-            for (int custo : custo_aresta){
-                if (cont == 0){
-                    menor_custo = custo;
-                    i_menor = cont;
-                }
-
-                if (menor_custo > custo){
-                menor_custo = custo;
-                i_menor = cont;
-                }
-
-                cont++;
-
-            }
-            
-        }
-    
-        s.sequencia.insert(s.sequencia.begin() + arestas[i_menor][0], arestas[i_menor][1]);
-
-        int indice_para_excluir;
-
-        for (i = 0; i < caminhos_disponiveis.size(); i++){
-            if (arestas[i_menor][1] == caminhos_disponiveis[i]){
-                indice_para_excluir = i;
-                break;
-            }
-        }
-
-        caminhos_disponiveis.erase(caminhos_disponiveis.begin() + indice_para_excluir);
-
-    }
-
-    return s;
-
-} 
-
 using namespace std;
 
 int main()
@@ -203,7 +203,7 @@ int main()
     int maxIterIls = 100;
 
     Solution solucao = {{}};
-    solucao = Construcao();
+    solucao.construcao();
     Solution best = solucao;
     best.exibirSolucao();
     cout << "Custo: " << best.calcularValorObj() << endl;
